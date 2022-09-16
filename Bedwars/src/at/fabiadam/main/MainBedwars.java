16742ca7a66056db12f68bf3cabf27565c7492d9
BuildTools <unconfigured@null.spigotmc.org>
2022-09-16 18:32:28 +0200

    feat!: add logic to MainBedwars.java - add the onEnable() function

--- Bedwars/src/at/fabiadam/main/MainBedwars.java
+++ Bedwars/src/at/fabiadam/main/MainBedwars.java
@@ -1,4 +1,10 @@
 package at.fabiadam.main;
 
-public class MainBedwars {
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class MainBedwars extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        System.out.println("Bedwars Plugin enabled!");
+    }
 }
