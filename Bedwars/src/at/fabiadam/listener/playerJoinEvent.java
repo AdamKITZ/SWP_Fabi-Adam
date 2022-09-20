9d76b5a1c7c41087062f100cc86aa4505ea0cddb
AdamKITZ <adam.aigner@hak-kitz.ac.at>
2022-09-20 12:39:31 +0200

    feat: add location to playerJoinEvent
    - add player teleported to "world" after joining server
    - add maxPlayerCount to message when changeing to "world_bedwars_l"

--- Bedwars/src/at/fabiadam/listener/playerJoinEvent.java
+++ Bedwars/src/at/fabiadam/listener/playerJoinEvent.java
@@ -1,5 +1,7 @@
 package at.fabiadam.listener;
 
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
@@ -8,6 +10,7 @@ import org.bukkit.event.player.PlayerJoinEvent;
 public class playerJoinEvent implements Listener {
     @EventHandler
     public void onPlayerJoinEvent(PlayerJoinEvent event) {
+        Location spawn = new Location(Bukkit.getWorld("world"), 180, 68, -48);
         event.getPlayer().sendMessage("§aWelcome to the Bedwars Server!\nYou can click on the sign to join the game lobby!");
     }
 }
--- Bedwars/src/at/fabiadam/listener/playerWorldChangeEvent.java
+++ Bedwars/src/at/fabiadam/listener/playerWorldChangeEvent.java
@@ -15,8 +15,8 @@ public class playerWorldChangeEvent implements Listener {
     public void onWorldChangeEvent(PlayerChangedWorldEvent event) {
         playerCount = Bukkit.getServer().getWorld("world_bedwars_l").getPlayers().size();
         if(event.getPlayer().getWorld().getName().equals("world_bedwars_l")) {
-            event.getPlayer().sendMessage("§bCurrent Players: " + playerCount + "\n§bPlayers required to start: " + minPlayerCount);
-            event.getPlayer().sendMessage("§aWelcome to the Bedwars lobby!\nYou can start a round by typing §6/start§a before the timer stops");
+            event.getPlayer().sendMessage("§bPlayers: " + playerCount + "/" + maxPlayerCount + "\n§bPlayers required to start: " + minPlayerCount);
+            event.getPlayer().sendMessage("§aYou can start a round by typing §6/start§a before the timer stops");
         }
     }
 }
