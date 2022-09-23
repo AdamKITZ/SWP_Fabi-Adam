7105ca37fc82882d97b06b69a363e3333cfd6fdf
BuildTools <unconfigured@null.spigotmc.org>
2022-09-23 07:54:10 +0200

    feat: add logic to several classes

--- Bedwars/src/at/fabiadam/commands/startRound.java
+++ Bedwars/src/at/fabiadam/commands/startRound.java
@@ -1,5 +1,6 @@
 package at.fabiadam.commands;
 
+import at.fabiadam.listener.playerWorldChangeEvent;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -11,8 +12,8 @@ public class startRound implements CommandExecutor {
         if(sender instanceof Player) {
             Player player = (Player) sender;
             if(player.hasPermission("bedwars.start")) {
-                if(player.getWorld().getName().equals("world_bedwars_lobby")) {
-
+                if(player.getWorld().getName().equals("world_bedwars_l")) {
+                    playerWorldChangeEvent.startGame();
                 } else {
                     player.sendMessage("§cYou can not do this on your current world!");
                 }
@@ -22,7 +23,6 @@ public class startRound implements CommandExecutor {
         } else {
             sender.sendMessage("§cYou can only do this as a player!");
         }
-
         return true;
     }
 }
