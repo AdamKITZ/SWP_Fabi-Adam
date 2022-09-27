0af03fc2b41444d384c1358f63350e617accc903
BuildTools <unconfigured@null.spigotmc.org>
2022-09-27 11:21:47 +0200

    feat: lobbyTimer.java

--- Bedwars/src/at/fabiadam/commands/startLobbyTimer.java
+++ Bedwars/src/at/fabiadam/commands/startLobbyTimer.java
@@ -6,14 +6,16 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
-public class startRound implements CommandExecutor {
+import static at.fabiadam.timers.lobbyTimer.startScheduler;
+
+public class startLobbyTimer implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if(sender instanceof Player) {
             Player player = (Player) sender;
             if(player.hasPermission("bedwars.start")) {
                 if(player.getWorld().getName().equals("world_bedwars_l")) {
-                    playerWorldChangeEvent.startGame();
+                    startScheduler(true);
                 } else {
                     player.sendMessage("§cYou can not do this on your current world!");
                 }
