b6d6a45e640f75b8a6da156e4a32142723ffe5aa
BuildTools <unconfigured@null.spigotmc.org>
2022-11-23 17:36:28 +0100

    fix: Scoreboard should work now
    -changes in Board.java & BedwarsUtil.java

--- Bedwars/src/at/fabiadam/scoreboard/Board.java
+++ Bedwars/src/at/fabiadam/scoreboard/Board.java
@@ -14,17 +14,17 @@ public class Board implements Listener {
     @EventHandler
     public void handlePlayerJoin(PlayerJoinEvent event) {
         Player player = event.getPlayer();
-        if(player.getWorld().getName().equals("world_bedwars_l")){
 
+        if(player.getWorld().getName().equals("world_bedwars_l")){
             Scoreboard board = Bukkit.getScoreboardManager().getNewScoreboard();
-            Objective object = board.registerNewObjective("s1","s2");
+            Objective object = board.registerNewObjective("test", "dummy");
 
             object.setDisplaySlot(DisplaySlot.SIDEBAR);
             object.setDisplayName("§a§Willkommen!");
 
             object.getScore("§aTest Line 3").setScore(3);
-            object.getScore("§c§lTest Line 4").setScore(4);
-            object.getScore("§a§hTest Line 5").setScore(5);
+            object.getScore("§c§lTest Line 4").setScore(2);
+            object.getScore("Test Line 5").setScore(1);
         }
 
     }
