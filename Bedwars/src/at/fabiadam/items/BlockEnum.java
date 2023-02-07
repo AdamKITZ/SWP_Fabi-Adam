e70b0f426d27b3cafda963b94cf18d249298aff6
stampFabian <fabian.trenkwalder@hak-kitz.ac.at>
2023-02-07 12:40:31 +0100

    feat: destroyable blocks

---
+++ Bedwars/src/at/fabiadam/items/BlockEnum.java
@@ -0,0 +1,22 @@
+package at.fabiadam.items;
+
+public enum BlockEnum {
+    SANSDSTONE, SAND;
+
+
+
+    /*
+    SANDSTONE
+    END_STONE
+    GLASS
+    PACKED_ICE
+    GLOWSTONE
+    INFESTED_STONE
+
+    IRON_BLOCK
+    CLAY_BLOCK
+    WOOD
+    SLIME_BLOCK
+    ACACIA_WOOD_STAIRS
+     */
+}
