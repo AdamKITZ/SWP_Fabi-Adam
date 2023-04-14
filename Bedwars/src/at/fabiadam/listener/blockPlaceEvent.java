90f7762de08209a780d4e1ad382b5b13b3b9a832
stampFabian <fabian.trenkwalder@hak-kitz.ac.at>
2023-04-14 07:59:29 +0200

    fix: several fixes

--- Bedwars/src/at/fabiadam/listener/blockPlaceEvent.java
+++ Bedwars/src/at/fabiadam/listener/blockPlaceEvent.java
@@ -1,13 +1,18 @@
 package at.fabiadam.listener;
 
+import at.fabiadam.items.BlockEnum;
 import at.fabiadam.main.MainBedwars;
 import at.fabiadam.util.BedwarsUtil;
 import org.bukkit.Bukkit;
 import org.bukkit.GameMode;
+import org.bukkit.Material;
 import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.Item;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockPlaceEvent;
+
+import javax.lang.model.element.Element;
 
 public class blockPlaceEvent implements Listener {
     private MainBedwars plugin;
@@ -27,7 +32,7 @@ public class blockPlaceEvent implements Listener {
         if (!(event.getPlayer().getWorld().getName().equals("world_bedwars")))  return;
         if (!(event.getPlayer().getGameMode() == GameMode.SURVIVAL)) return;
         if (util.getSpawnBlocks(config.getLocation("team.red.teamSpawn")).contains(event.getBlock().getLocation())) {
-            event.setCancelled(true);
+                event.setCancelled(true);
         } else if (util.getSpawnBlocks(config.getLocation("team.blue.teamSpawn")).contains(event.getBlock().getLocation())) {
             event.setCancelled(true);
         } else if (util.getSpawnBlocks(config.getLocation("team.green.teamSpawn")).contains(event.getBlock().getLocation())) {
@@ -35,7 +40,5 @@ public class blockPlaceEvent implements Listener {
         } else if (util.getSpawnBlocks(config.getLocation("team.yellow.teamSpawn")).contains(event.getBlock().getLocation())) {
             event.setCancelled(true);
         }
-
-
     }
 }
