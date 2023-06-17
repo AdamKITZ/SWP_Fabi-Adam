ce3e548e4c9f88618ebc63b65ee8a72be1acd92d
AdamKITZ <adam.aigner@hak-kitz.ac.at>
2023-06-17 02:19:28 +0200

    fix: 100%

--- Bedwars/src/at/fabiadam/listener/blockPlaceEvent.java
+++ Bedwars/src/at/fabiadam/listener/blockPlaceEvent.java
@@ -12,7 +12,6 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockPlaceEvent;
 
-import javax.lang.model.element.Element;
 
 public class blockPlaceEvent implements Listener {
     private MainBedwars plugin;
--- Bedwars/src/at/fabiadam/util/ShopItems.java
+++ Bedwars/src/at/fabiadam/util/ShopItems.java
@@ -116,7 +116,7 @@ public class ShopItems {
         //Knockbackstick
         ItemMeta stickMeta = stick.getItemMeta();
         stick.addUnsafeEnchantment(Enchantment.KNOCKBACK, 3);
-        stick.setItemMeta(stickMeta);
+        //stick.setItemMeta(stickMeta);
         ItemMeta stickNewMeta = stick.getItemMeta();
         stickNewMeta.setDisplayName("1 Knockback Stick 7 Bronze");
         stick.setItemMeta(stickNewMeta);
